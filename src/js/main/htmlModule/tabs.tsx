79e48a4163472ff0035fa4bbe745f55b335fd79b
ENNA-Egor <e.yudakov@gmail.com>
2023-08-15 23:10:53 +0300

    Добавил ещё одну панель полигонов

--- src/js/main/htmlModule/tabs.tsx
+++ src/js/main/htmlModule/tabs.tsx
@@ -21,8 +21,32 @@ import React from "react";
                     <div className="button button_minus"></div>
                 </div>
             </fieldset>   
+
+        <div className="tab-polygon tab-b">
+            <fieldset className="field">
+               <legend>Polygon</legend>
+               <div className="polygon-blok point-blok">
+               <p className="n_points">Number Points</p>
+               <input type="number" className="n_points  inp" name="num_points" min="3" max="15" value="5"/>
+               </div>
+               <div className="polygon-blok">
+                  <div className="button buttonP">Create</div>
+               </div>  
+               <p>Cap</p>
+               <div className="linecap_blok" >
+                  <div className="button button_cap  but"></div>
+                  <div className="button button_cap round"></div>
+               </div> 
+               <p>Add/End</p>
+               <div className="add_end_blok" >
+                  <div className="button button_plus"></div>
+                  <div className="button button_minus"></div>
+               </div>
+            </fieldset>
+         </div>
         </div>   
     </div> 
+
         );
      }  
 
