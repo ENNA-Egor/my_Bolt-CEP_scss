0aa37f48691b1d234e2dc9af947631b1cc168d68
ENNA-Egor <e.yudakov@gmail.com>
2023-09-12 16:11:01 +0300

    Добавил ещё одну панель

---
+++ src/js/colorsett/index.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { initBolt } from "../lib/utils/bolt";
+// // import "../main/scss/index.scss";
+// // import "../../../css/index.css";
+// import "../main/scss/index.css";
+// import Main from "./main";
+// import Tabs from '../main/htmlModule/tabs'
+
+
+initBolt();
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+      <h1>Color Palette</h1>
+  </React.StrictMode>
+);
