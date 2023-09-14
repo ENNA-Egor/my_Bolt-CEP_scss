98a26540efebb66ee979f7d7298af69afc680e30
ENNA-Egor <e.yudakov@gmail.com>
2023-09-14 21:54:31 +0300

    Crefte Main

--- src/js/settings/index.tsx
+++ src/js/settings/index.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { initBolt } from "../lib/utils/bolt";
-// // import "../main/scss/index.scss";
+import "../main/scss/index.scss";
 // // import "../../../css/index.css";
-import "../main/scss/index.css";
-// import Main from "./main";
+// import "../main/scss/index.css";
+import Main from "./main";
 // import Tabs from '../main/htmlModule/tabs'
 
 
@@ -12,11 +12,6 @@ initBolt();
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <div className="boddy">
-      <p>Color Palette</p>
-      <div className="buttons">
-      <div className="button">Click</div>
-      </div>
-    </div>
+    <Main/>
   </React.StrictMode>
 );
---
+++ src/js/settings/main.tsx
@@ -0,0 +1,43 @@
+import { useEffect, useState } from "react";
+import { os, path } from "../lib/cep/node";
+import {
+  csi,
+  evalES,
+  evalFile,
+  openLinkInBrowser,
+  subscribeBackgroundColor,
+  evalTS,
+} from "../lib/utils/bolt";
+
+// import bolt from "../assets/bolt-cep.svg";
+import circle from "../assets/png/Circle-01.png";
+import polygon from "../assets/png/Polygon-01.png";
+import color from "../assets/png/Color.png";
+import setting from "../assets/png/Help.png";
+
+import "../main/scss/main.scss";
+// import "../main/scss/main.css";
+// import "../../../css/index.css";
+
+const Main = () => {
+  const [bgColor, setBgColor] = useState("#282c34");
+  
+  useEffect(() => {
+    if (window.cep) {
+      subscribeBackgroundColor(setBgColor);
+    }
+  }, []);
+
+
+
+  return (
+   <div className="boddy">
+   <p>Color Palette</p>
+   <div className="buttons">
+   <div className="button">Click</div>
+   </div>
+ </div>
+  );
+};
+
+export default Main;
