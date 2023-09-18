422b3cf09f935c67d3ff645d2e5ba477d9048737
ENNA-Egor <e.yudakov@gmail.com>
2023-09-18 14:22:40 +0300

    Исправил разметку на вкладке Setting

--- src/js/main/htmlModule/Tab_setting.tsx
+++ src/js/main/htmlModule/Tab_setting.tsx
@@ -4,8 +4,8 @@ function Tab_setting(){
         <fieldset className="field">
            <legend>Settings</legend>
            <div className="sett-bloc">
-              <div className="sett1">
-                 <p>Numb element</p>
+              <div className="sett">
+                 <p>N_element</p>
                  <input type="number" className="n_elem inp" name="num-element" min="2" max="15" value="5"/>
               </div>
               <div className="sett">
@@ -14,20 +14,20 @@ function Tab_setting(){
                  <input type="number" className="resol resol-h  inp" name="resolution" value="1080"/>
               </div>
               <div className="sett">
-                 <p>Frame Rate   </p>
+                 <p>F/R   </p>
                  <input type="number" className="framerate n_points inp" name="framerate" value="25"/>
               </div>
               <div className="sett">
-                 <p>Duranion (sek)</p>
+                 <p>Dur(sek)</p>
                  <input type="number" className="duration n_points inp" name="duration" value="6"/>
                  <input type="checkbox" checked className="n_points one_dur" name="check_color_pick"/>
               </div>
               <div className="sett">
-                 <p>Influence ( % )</p>
+                 <p>Inf( % )</p>
                  <input type="number" className="influ n_points inp" name="influence" value="90"/>
               </div>
               <div className="sett">
-              <div className="button bt7">Setting Comp</div>
+              <div className="button bt7">SetComp</div>
               </div>
            </div>
         </fieldset>
