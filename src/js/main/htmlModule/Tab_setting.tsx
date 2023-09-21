d03b2a66fac44fe8b92b1e6a347deebeae1d521e
ENNA-Egor <e.yudakov@gmail.com>
2023-09-21 21:32:12 +0300

    Заменил инпут number на text

--- src/js/main/htmlModule/Tab_setting.tsx
+++ src/js/main/htmlModule/Tab_setting.tsx
@@ -6,25 +6,25 @@ function Tab_setting(){
            <div className="sett-bloc">
               <div className="sett">
                  <p>N_element</p>
-                 <input type="number" className="n_elem inp" name="num-element" min="2" max="15" value="5"/>
+                 <input type="text" className="n_elem inp" name="num-element" min="2" max="15" value="5"/>
               </div>
               <div className="sett">
                  <p>Res</p>
-                 <input type="number" className="resol resol-w  inp2" name="resolution" value="1920"/>
-                 <input type="number" className="resol resol-h  inp2" name="resolution" value="1080"/>
+                 <input type="text" className="resol resol-w  inp2" name="resolution" value="1920"/>
+                 <input type="text" className="resol resol-h  inp2" name="resolution" value="1080"/>
               </div>
               <div className="sett">
                  <p>F/R   </p>
-                 <input type="number" className="framerate n_points inp" name="framerate" value="25"/>
+                 <input type="text" className="framerate n_points inp" name="framerate" value="25"/>
               </div>
               <div className="sett dur_set">
                  <p>Dur(sek)</p>
-                 <input type="number" className="duration n_points inp" name="duration" value="6"/>
+                 <input type="text" className="duration n_points inp" name="duration" value="6"/>
                  <input type="checkbox" checked className="n_points one_dur" name="check_one-dur"/>
               </div>
               <div className="sett ">
                  <p>Inf( % )</p>
-                 <input type="number" className="influ n_points inp" name="influence" value="90"/>
+                 <input type="text" className="influ n_points inp" name="influence" value="90"/>
               </div>
               <div className="sett">
               <div className="button bt7">SetComp</div>
