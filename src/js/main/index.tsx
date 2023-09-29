fed6d7e165b8c546d2a2a840af318baabd847560
ENNA-Egor <e.yudakov@gmail.com>
2023-09-29 21:20:00 +0300

    Настроил расположение папок компилятора sass.

--- src/js/main/index.tsx
+++ src/js/main/index.tsx
@@ -1,9 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { initBolt } from "../lib/utils/bolt";
-import "../main/scss/index.scss";
-// import "../../../css/index.css";
-// import "../main/scss/index.css";
+// import "../main/scss/index.scss";
+import "../main/style/css/min/index.min.css";
 import Main from "./main";
 import Tabs from '../main/htmlModule/tabs'
 
