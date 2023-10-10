478f63fabddb9f28c80e513c33e3f1428cbdf173
ENNA-Egor <e.yudakov@gmail.com>
2023-10-10 14:43:16 +0300

    Перенёс папку style

--- src/js/main/index.tsx
+++ src/js/main/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { initBolt } from "../lib/utils/bolt";
-import "../style/css/min/index.min.css";
+import "../../style/css/min/index.min.css";
 import Main from "./main";
 import Tabs from '../main/htmlModule/tabs'
 
