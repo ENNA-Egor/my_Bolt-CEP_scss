0ea61c9fc9d411a3eab09bf81a366060346e19b2
ENNA-Egor <e.yudakov@gmail.com>
2024-03-04 13:44:42 +0300

    Заменил defaultValue на value

--- src/js/main/htmlModule/Tab_setting.tsx
+++ src/js/main/htmlModule/Tab_setting.tsx
@@ -82,21 +82,21 @@ import {
                    </div>
                    <div className="sett">
                       <p>Res</p>
-                      <input  onChange={this.changeInput} type="text" className="resol resol-w  inp2" name="resolution_w" defaultValue={this.state.resolution_w}/>
-                      <input  onChange={this.changeInput} type="text" className="resol resol-h  inp2" name="resolution_h" defaultValue={this.state.resolution_h}/>
+                      <input  onChange={this.changeInput} type="text" className="resol resol-w  inp2" name="resolution_w" value={this.state.resolution_w}/>
+                      <input  onChange={this.changeInput} type="text" className="resol resol-h  inp2" name="resolution_h" value={this.state.resolution_h}/>
                    </div>
                    <div className="sett">
                       <p>F/R   </p>
-                      <input  onChange={this.changeInput} type="text" className="framerate n_points inp" name="framerate" defaultValue={this.state.framerate}/>
+                      <input  onChange={this.changeInput} type="text" className="framerate n_points inp" name="framerate" value={this.state.framerate}/>
                    </div>
                    <div className="sett dur_set">
                       <p>Dur(sek)</p>
-                      <input  onChange={this.changeInput} type="text" className="duration n_points inp" name="duration" defaultValue={this.state.duration}/>
+                      <input  onChange={this.changeInput} type="text" className="duration n_points inp" name="duration" value={this.state.duration}/>
                       <input  onChange={this.changeInput} type="checkbox" checked className="n_points one_dur" name="check_one-dur"/>
                    </div>
                    <div className="sett ">
                       <p>Inf( % )</p>
-                      <input  onChange={this.changeInput} type="text" className="influ n_points inp" name="influence" defaultValue={this.state.influence}/>
+                      <input  onChange={this.changeInput} type="text" className="influ n_points inp" name="influence" value={this.state.influence}/>
                    </div>
                    <div className="sett">
                    <div className="button bt7" onClick={this.compCustomSettings}>SetComp</div>
