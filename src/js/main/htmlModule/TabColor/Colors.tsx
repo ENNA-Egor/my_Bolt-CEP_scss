690ed969a070ea8ebcd5de6a818f32f9567e03b5
ENNA-Egor <e.yudakov@gmail.com>
2024-03-27 11:17:25 +0300

    Надо данные из массива получить через фор ич

--- src/js/main/htmlModule/TabColor/Colors.tsx
+++ src/js/main/htmlModule/TabColor/Colors.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import ColorPal from "./Color";
 
-const colorArr = ["d5e390","628f2e","14dcdc","fafa98","1f8a70","bedb39","ffff1a","7f7fff","c1faec","d5e390","628f2e","14dcdc","fafa98","1f8a70","bedb39"];
 
 const Colors = ()=> {
-
+  
+  let colorArr=[]; 
+  colorArr= ["d5e390","628f2e","14dcdc","fafa98","1f8a70","bedb39","ffff1a","7f7fff","c1faec","d5e390","628f2e","14dcdc","fafa98","1f8a70","bedb39"];
     return (
         <div className="blok_palet">
         <ColorPal color='red'/>
@@ -22,6 +23,11 @@ const Colors = ()=> {
         <ColorPal color='green'/>
         <ColorPal color='aquamarine'/>
         <ColorPal color='rgb(115, 209, 60)'/>
+        {/* {
+          colorArr.map(color => {
+            return <Color {color}
+          })
+        } */}
       </div>   
     );
 };
