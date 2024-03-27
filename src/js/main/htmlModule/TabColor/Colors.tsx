0e977b717a0c4ed0b943b3be7bf81a890d1a234c
ENNA-Egor <e.yudakov@gmail.com>
2024-03-27 16:23:18 +0300

    наполнение из массива. Надо решить вопрос с отрисовкой

--- src/js/main/htmlModule/TabColor/Colors.tsx
+++ src/js/main/htmlModule/TabColor/Colors.tsx
@@ -5,30 +5,16 @@ import ColorPal from "./Color";
 
 const Colors = ()=> {
   
-  const colorArr: Array <string>= ["red","rgb(115, 209, 60)","white","yellow","1f8a70","bedb39","ffff1a","7f7fff","c1faec","d5e390","628f2e","14dcdc","fafa98","1f8a70","bedb39"];
- console.log (String(colorArr[0]))
+  const colorArr: Array <string>= ["red","rgb(115, 209, 60)","white","yellow","1f8a70","bedb39","green","7f7fff","c1faec","d5e390","628f2e","14dcdc","fafa98","1f8a70","red"];
   return (
-        <div className="blok_palet">
-        <ColorPal color={colorArr[0]}/>
-        <ColorPal color={colorArr[1]}/>
-        <ColorPal color={colorArr[2]}/>
-        <ColorPal color={colorArr[3]}/>
-        <ColorPal color='rgb(115, 209, 60)'/>
-        <ColorPal color='red'/>
-        <ColorPal color='blue'/>
-        <ColorPal color='green'/>
-        <ColorPal color='aquamarine'/>
-        <ColorPal color='rgb(115, 209, 60)'/>
-        <ColorPal color='red'/>
-        <ColorPal color='blue'/>
-        <ColorPal color='green'/>
-        <ColorPal color='aquamarine'/>
-        <ColorPal color='rgb(115, 209, 60)'/>
-        {
-          // colorArr.map(color => {
-          //   return <Color {color}
-          // })
-        }
+    <div className="blok_palet">
+          {
+            colorArr.map(color => {
+              console.log (color)
+              return  <ColorPal color={color}/>
+            })
+          }
+        
       </div>   
     );
 };
