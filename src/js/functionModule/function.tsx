e2c019308b756d3f1c5d33e1583002bb424b4072
ENNA-Egor <e.yudakov@gmail.com>
2024-04-05 08:48:38 +0300

    Добавил функцию rgbToHex

--- src/js/functionModule/function.tsx
+++ src/js/functionModule/function.tsx
@@ -10,6 +10,19 @@ export function hexToRgb(hex){
   }
 
 
+export function rgbToHex(resultn: string) {
+
+   var resultColor: string[] = resultn.split(',');
+     //@ts-ignore
+     var r = parseInt(resultColor[0]*255);
+      //@ts-ignore
+     var g = parseInt(resultColor[1]*255);
+      //@ts-ignore
+     var b = parseInt(resultColor[2]*255);
+   return "0x" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+ }  
+
+
   export function writeColorData (colors){
 
     const saveFolderPath = path.join(__dirname, '/settingsData');
