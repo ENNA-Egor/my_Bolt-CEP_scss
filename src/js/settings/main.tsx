3576dcfdb52435a1affe316480b711fd3d54a954
ENNA-Egor <e.yudakov@gmail.com>
2024-05-14 22:06:02 +0300

    Начал создание панели цвутов

--- src/js/settings/main.tsx
+++ src/js/settings/main.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { HtmlHTMLAttributes, useEffect, useState } from "react";
 import { os, path } from "../lib/cep/node";
 import {
   csi,
@@ -22,10 +22,23 @@ const Main = () => {
 
   return (
    <div className="boddy">
-   <p>Color Palette</p>
-   <div className="buttons">
+   {/* <p>Color Palette</p> */}
+   {/* <div className="buttons">
    <div className="button">Click</div>
-   </div>
+   </div> */}
+   {/* <h3>Color palette</h3> */}
+     {/* <div>
+        <button class="butt btn">Add</button>
+    </div> */}
+    <div>
+        <fieldset className="field_1">
+            <legend className="legend">Add Preset</legend>
+            <button className="button_bt btn2">+</button>
+            <input className="setName" type="text" placeholder="Enter a group name"/>
+             {/* <button class="button btn3">- Data</button>
+            <button class="button btn4">^ Data</button> */}
+        </fieldset>
+    </div>
  </div>
   );
 };
