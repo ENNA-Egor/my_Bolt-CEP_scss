7b56b54fc5c938209106dc1b9cf3ed7eb17840e2
ENNA-Egor <e.yudakov@gmail.com>
2024-05-30 22:18:34 +0300

    Проба 2

--- src/js/settings/PalettContext.tsx
+++ src/js/settings/PalettContext.tsx
@@ -15,12 +15,12 @@ export const Context = (props) => {
                 Arbitrary:["2f6b6b","2f5151","104646","6cb7b7","80b7b7","7f7fff","c1faec","ba8db6","7f44eb","004358","ffff1a","1f8a70","bedb39","ffff1a","7f7fff"]
             } ,
             Custom:{
-                // Glow: ["9a9afa", "c9b1f4", "91a679"],
-                // Gol: ["5b82b3", "86cab9", "d5e390", "fafa98", "c1faec"],
-                //  New_6: ["d5e390", "628f2e", "14dcdc", "fafa98", "1f8a70", "bedb39", "ffff1a", "7f7fff", "c1faec", "d5e390", "628f2e", "14dcdc", "fafa98", "1f8a70", "bedb39"],
-                //  Start2: ["7f44eb", "628f2e", "92fce3"],
-                //  monocromRed: ["fbc9c9", "fb9797", "fb6464", "fb3232", "fb0000"],
-                //  New: ["5b82b3", "86cab9", "d5e390", "fafa98", "fabb86"]
+                Glow: ["9a9afa", "c9b1f4", "91a679"],
+                Gol: ["5b82b3", "86cab9", "d5e390", "fafa98", "c1faec"],
+                 New_6: ["d5e390", "628f2e", "14dcdc", "fafa98", "1f8a70", "bedb39", "ffff1a", "7f7fff", "c1faec", "d5e390", "628f2e", "14dcdc", "fafa98", "1f8a70", "bedb39"],
+                 Start2: ["7f44eb", "628f2e", "92fce3"],
+                 monocromRed: ["fbc9c9", "fb9797", "fb6464", "fb3232", "fb0000"],
+                 New: ["5b82b3", "86cab9", "d5e390", "fafa98", "fabb86"]
             }
         }
     )
