f8870e317a3df2a5f15cc52ecde198668e2ad359
ENNA-Egor <e.yudakov@gmail.com>
2024-06-27 09:06:27 +0300

    Очистил содержание customPalett

--- src/js/colorsPalett/hooks/PalettContext.tsx
+++ src/js/colorsPalett/hooks/PalettContext.tsx
@@ -13,53 +13,53 @@ export const Context = (props) => {
                 group: "",
                 colors: []
             },
-            {   id: 2,
-                name:  "Start",
-                group: "Default",
-                colors: [ "004358", "1f8a70","bedb39","ffff1a","7f7fff"]
-            },
+            // {   id: 2,
+            //     name:  "Start",
+            //     group: "Default",
+            //     colors: [ "004358", "1f8a70","bedb39","ffff1a","7f7fff"]
+            // },
 
-            {   id: 3,
-                name:  "Bright",
-                group: "Default",
-                colors:[ "7f44eb","628f2e","92fce3", "ff00ff" ]
-            }, 
-            {   
-                id: 4,
-                name:  "Arbitrary",
-                group: "Custom",
-                colors: ["2f6b6b","2f5151","104646","6cb7b7","80b7b7","7f7fff","c1faec","ba8db6", "7f44eb","004358","ffff1a","1f8a70","bedb39","ffff1a","7f7fff"]
-            }, 
-            {   id: 5,
-                name:  "Glow",
-                group: "Main",
-                colors:[ "9a9afa", "c9b1f4","91a679"]
-            },
-            {   id: 6,
-                name:  "Gold",
-                group: "Custom",
-                colors:[ "5b82b3", "86cab9","d5e390","fafa98","c1faec"]
-            },
-            {   id: 7,
-                name:  "New_6",
-                group: "Main",
-                colors:[ "d5e390","628f2e","14dcdc","fafa98","1f8a70","bedb39","ffff1a","7f7fff","c1faec","d5e390","628f2e","14dcdc","fafa98","1f8a70","bedb39"]
-            },
-            {   id: 8,
-                name:  "Start2",
-                group: "Main",
-                colors: [ "7f44eb","628f2e","92fce3"]
-            },
-            {   id: 9,
-                name:  "monocromRed",
-                group: "Main",
-                colors:["fbc9c9","fb9797","fb6464", "fb3232","fb0000"]
-            },
-            {   id: 10,
-                name:  "New",
-                group: "Main",
-                colors:[ "5b82b3","86cab9","d5e390","fafa98","fabb86"]
-            }
+            // {   id: 3,
+            //     name:  "Bright",
+            //     group: "Default",
+            //     colors:[ "7f44eb","628f2e","92fce3", "ff00ff" ]
+            // }, 
+            // {   
+            //     id: 4,
+            //     name:  "Arbitrary",
+            //     group: "Custom",
+            //     colors: ["2f6b6b","2f5151","104646","6cb7b7","80b7b7","7f7fff","c1faec","ba8db6", "7f44eb","004358","ffff1a","1f8a70","bedb39","ffff1a","7f7fff"]
+            // }, 
+            // {   id: 5,
+            //     name:  "Glow",
+            //     group: "Main",
+            //     colors:[ "9a9afa", "c9b1f4","91a679"]
+            // },
+            // {   id: 6,
+            //     name:  "Gold",
+            //     group: "Custom",
+            //     colors:[ "5b82b3", "86cab9","d5e390","fafa98","c1faec"]
+            // },
+            // {   id: 7,
+            //     name:  "New_6",
+            //     group: "Main",
+            //     colors:[ "d5e390","628f2e","14dcdc","fafa98","1f8a70","bedb39","ffff1a","7f7fff","c1faec","d5e390","628f2e","14dcdc","fafa98","1f8a70","bedb39"]
+            // },
+            // {   id: 8,
+            //     name:  "Start2",
+            //     group: "Main",
+            //     colors: [ "7f44eb","628f2e","92fce3"]
+            // },
+            // {   id: 9,
+            //     name:  "monocromRed",
+            //     group: "Main",
+            //     colors:["fbc9c9","fb9797","fb6464", "fb3232","fb0000"]
+            // },
+            // {   id: 10,
+            //     name:  "New",
+            //     group: "Main",
+            //     colors:[ "5b82b3","86cab9","d5e390","fafa98","fabb86"]
+            // }
         ]
 
     )
